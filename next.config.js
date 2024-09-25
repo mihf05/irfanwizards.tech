4e47328318207f6b51ecb75d89a7177795e0d69c
Md Irfan Hasan Fahim <81842071+mihf05@users.noreply.github.com>
2024-09-25 05:47:09 +0000

    Configured for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-    output: 'export',
-    // other configurations...
-  }
-  
-  module.exports = nextConfig
+  output: 'export',
+  assetPrefix: isProd ? '/irfanwizards.tech/' : '',
+  basePath: isProd ? '/irfanwizards.tech' : '',
+  images: {
+    unoptimized: true // Disable image optimization for GitHub Pages
+  },
+  // other configurations...
+}
+
+module.exports = nextConfig;
